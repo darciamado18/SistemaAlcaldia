4dbcfacf9eedfd135690f01cdb0fa240ac67b4f7
LAPTOP-27E27LTJ\darci <darciamado18@gmail.com>
2022-06-14 17:32:20 -0500

    no message

--- Vista/loguin.php
+++ Vista/loguin.php
@@ -4,51 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>loguin</title>
-    <link href="../bootstrap/bootstrap.css" rel="stylesheet">
-  
+    <link rel="stylesheet" href="../scss/style.scss"
+    <title>INICIO DE SECION</title>
 </head>
-<body background="../imagenes/saman.jpg" width = "500">
-<div class="container w-100  mt-5 mb-5 ">
-
-                 
+<body>
+    <section class="container">
             <div class="login">
-                <h1>INICIO DE SESIÓN</h1> 
-                   <form action="../controlador/controlador.php" method="post">                        
-                        <p> <input for="frol" class="form-label">ROL:
-                            <select name="frol">
-                            <option selected value="0"> Elige una opción </option>
-                            <optgroup label="rol_"> 
-                            <option value="1">Administrador </option>
-                            <option value="2">Usuario       </option>
-                            </p>
-                                           
-                        <p> CORREO ELECTRONICO: <input type="text" name="fcorreoUsu" placeholder="nombre@sucorreo.com"> 
-                        </p>                       
-                      
-                        <p>CONTRASEÑA:<input type="password"  name="fpasswordUsu" placeholder="***********"> </p>
-                        <p class="submit"><center> <input type="submit" name="fboton" value="INGRESAR"></center> </p>
-                   </form>
-                   <?php
-                   /* @$mensaje = $_GET['mensaje'];
-                    if (isset($mensaje)){
-                    if($mensaje=='incorrecto'){
-                            echo '<div  class="alert alert-danger" role="alert"> usuario o clave incorrecto</div>';
-                        }
-
-                    }*/
-                    ?>
-</div>
-
-               <div class="row mt-4 " >
-                   <img src="../imagenes/aprobado.png" width="200" >
-               </div>
-           </div>
-           <div class="col-1 col-md-3 col-lg-4"></div>
-       </div>
-       
-   </div>
-
-   <script src="../bootstrap/bootstrap.bundle.js"></script>    
+                <h1>Login</h1>
+                <form method="post" action="mdloguin.php">
+                    <p><input type="text" name="fcorreoUsu" 
+                              placeholder="Ingrese Usuario"></p>
+                    <p><input type="password" name="fpasswordUsu" 
+                              placeholder="Contrase&ntilde;a"></p>
+                    <p class="remember_me">
+                        <label>
+                            <input type="checkbox" name="recordarme" 
+                                   value="false">
+                            Recordar mis datos
+                        </label>
+                    </p>
+                    <p class="submit"><input type="submit" value="Ingresar"></p>
+                </form>
+            </div>
+    </section>
+    
 </body>
-</html>
+</html>l
