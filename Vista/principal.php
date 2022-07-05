57a555f13ad8fc35b8910f2688ba1b916bc25cf3
LAPTOP-27E27LTJ\darci <darciamado18@gmail.com>
2022-07-05 15:52:01 -0500

    no message

--- Vista/principal.php
+++ Vista/principal.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PRINCIPAL</title>
     <link href="../boodstrap/bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/hoja.css" >
+    <link rel="stylesheet" href="../css/style.css" >
 </head>
 <body>
   <?php 
