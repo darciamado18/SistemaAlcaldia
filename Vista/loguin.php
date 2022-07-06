c60a2dacb0b1a06dfe39b050593cd71fd96c2ea0
LAPTOP-27E27LTJ\darci <darciamado18@gmail.com>
2022-07-06 11:14:15 -0500

    no message

--- Vista/loguin.php
+++ Vista/loguin.php
@@ -5,33 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>loguin</title>
-    <link rel="stylesheet" href="../css/estiloLoguin.css"
-    
+    <link rel="stylesheet" href="../css/estiloLoguin.css"    
 </head>
 
 <body background="../imagenes/landazuri.jpg">
-    <div class="container ">        
-           <div class="row  ">              
+    <div class="container">        
+           <div class="row">              
 
-                   <h1 class="text-center"> BIENVENIDO A LA PLATAFORMA ALCALDIA </h1>
+                   <h1 class="text-center text-success"> BIENVENIDO A LA PLATAFORMA ALCALDIA </h1>
                   
                    <br>
 
                    <form action="../controlador/controlador.php" method="post">
+
+                       <div>
+                            <label for="rol" class="form-label"></label>
+                       </div>
                        <div class="mb-2">
-                           <label for="email" class="form-label">CORREO ELECTRONICO</label>
-                           <input type="email" class="form-control" name="femail"> 
+                           <label for="correoUsu" class="form-label">CORREO ELECTRONICO</label>
+                           <input type="email" class="form-control" name="fcorreoUsu" id="correoUsu"> 
                        </div>
                        <br>
                        <div class="mb-2">
-                           <label for="password" class="form-label">CONTRASEÑA: </label>
-                           <input type="password" place class="form-control" name="fcontraseña"> 
+                           <label for="passwordUsu" class="form-label">CONTRASEÑA: </label>
+                           <input type="password" place class="form-control" name="fpasswordUsu" id="passwordUsu"> 
                        </div>
                         <br>
-                       <div class="d-grid">
-                           <button type="submit" class="btn btn-primary botones" name="fboton" value="ingresar">INICIAR SESION</button>
+                       <div >
+                           <button type="submit" class="btn btn-success" name="fboton" value="ingresar">INICIAR SESION</button>
                        </div>
                    </form>
 
+
+                   <?php
+                    @$mensaje = $_GET['mensaje'];
+                    if (isset($mensaje)){
+                    if($mensaje=='incorrecto'){
+                            echo '<div  class="alert alert-danger" role="alert"> usuario o clave incorrecto</div>';
+                        }
+
+                    }
+                    ?>
+            </div>               
+    </div>
+ 
 </body>
 </html>
