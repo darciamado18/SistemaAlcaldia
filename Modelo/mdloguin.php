ea14c7bf4f127d9149b28f32988d70faa165cf2c
LAPTOP-27E27LTJ\darci <darciamado18@gmail.com>
2022-07-19 17:36:54 -0500

    no message

--- Modelo/mdloguin.php
+++ Modelo/mdloguin.php
@@ -20,14 +20,14 @@
 
         function verificar(){ 
             $verificar = mysqli_query( $this->_conexion, "SELECT u.rol , u.correoUsu , u.passwordUsu , d.nombre1_Usu
-            FROM datospersonalesUsu D INNER JOIN  usuarioAlcaldia U ON u.idusuarioAlcaldia = iddatospersonalesUsu 
+            FROM usuarioAlcaldia u INNER JOIN datospersonalesUsu d ON u.idusuarioAlcaldia = d.iddatospersonales 
             WHERE  u.rol = '$this->_rol' AND u.correoUsu = '$this->_correoUsu' AND u.passwordUsu = '$this->_passwordUsu'   ")
             or die(mysqli_error( $this->_conexion));
                        
             if(mysqli_num_rows($verificar)){
                 $unusuario = mysqli_fetch_array($verificar);
-               /* $this->_idusuario   = $unusuario["idusuariosAlcaldia"];
-                $this->_iddatos     = $unusuario["iddatosPersonales"];*/    
+                $this->_idusuario   = $unusuario["idusuariosAlcaldia"];
+                $this->_iddatos     = $unusuario["iddatosPersonales"];
                 $this->_rol         = $unusuario["rol"];
                 $this->_correoUsu   = $unusuario["correoUsu"];
                 $this->_passwordUsu = $unusuario["passwordUsu"];
