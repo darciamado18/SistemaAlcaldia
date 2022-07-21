d9c0e5efc0b3e4233f73b603d9919967b56ce4d9
LAPTOP-27E27LTJ\darci <darciamado18@gmail.com>
2022-07-21 10:21:19 -0500

    no message

--- Modelo/mdloguin.php
+++ Modelo/mdloguin.php
@@ -19,28 +19,27 @@
         
 
         function verificar(){ 
-            $verificar = mysqli_query( $this->_conexion, "SELECT u.rol , u.correoUsu , u.passwordUsu , d.nombre1_Usu
+            $verificar = mysqli_query( $this->_conexion, "SELECT u.idusuarioAlcaldia, d.iddatospersonales, u.rol , u.correoUsu , u.passwordUsu , d.nombre1_Usu
             FROM usuarioAlcaldia u INNER JOIN datospersonalesUsu d ON u.idusuarioAlcaldia = d.iddatospersonales 
-            WHERE  u.rol = '$this->_rol' AND u.correoUsu = '$this->_correoUsu' AND u.passwordUsu = '$this->_passwordUsu'   ")
+            WHERE  u.rol = '$this->_rol' AND u.correoUsu = '$this->_correoUsu' AND u.passwordUsu = '$this->_passwordUsu' ")
             or die(mysqli_error( $this->_conexion));
                        
             if(mysqli_num_rows($verificar)){
                 $unusuario = mysqli_fetch_array($verificar);
-                $this->_idusuario   = $unusuario["idusuariosAlcaldia"];
+                $this->_idusuario   = $unusuario["idusuarioAlcaldia"];
                 $this->_iddatos     = $unusuario["iddatosPersonales"];
                 $this->_rol         = $unusuario["rol"];
                 $this->_correoUsu   = $unusuario["correoUsu"];
                 $this->_passwordUsu = $unusuario["passwordUsu"];
-                $this->_nombre1_Usu = $unusuario["nombre1_Usu"];
+                $this->_nombre1_Usu = $unusuario["nombre1_Usu"];    
                 
                 return $this->_idusuario;           
             }
-            return $this->_idusuario;
-           
+            return $this->_idusuario;           
         }
+        
        function getNombre(){
-            return $this-> _nombre1_Usu;
-            
+            return $this-> _nombre1_Usu;            
         }
    }
 ?> 
