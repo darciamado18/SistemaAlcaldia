03a859b2b26dec99c65513ef2a6445e144258f28
LAPTOP-27E27LTJ\darci <darciamado18@gmail.com>
2022-08-12 16:48:43 -0500

    no message

--- Vista/principal.php
+++ Vista/principal.php
@@ -7,16 +7,19 @@
     <title>PRINCIPAL</title>
     <link href="../boodstrap/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/estiloejemplo.css" >
-
     
 </head>
 <body background ="../imagenes/landazuri.jpg">
+
   <?php 
   session_start();
   @$nom = $_SESSION["nombre1_Usu"];
   ?>
+
 <nav class="navbar navbar-expand-lg navbar-light fondodegradado " >
+
       <form action="cerrarsesion.php" method="post">
+
               <div class="container-fluid">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav me-auto mb-3 mb-lg-0">
@@ -25,11 +28,12 @@
                       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         REGISTRAR
                       </a>
+
                       <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="registrarUsuarios.php">Registar Usuarios</a></li>
-                        
+                        <li><a class="dropdown-item" href="registrarUsuarios.php">Registar Usuarios</a></li>                        
                       </ul>
                     </li>
+
                     <li class="nav-item dropdown" >
                       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Prestar
@@ -48,36 +52,21 @@
                         <li><a class="dropdown-item" href="devolverComputador.php">Devolver Equipo</a></li>
                       </ul>
                     </li>
-                    <li class="nav-item dropdown" >
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Tablas
-                      </a>
-                      <ul class="dropdown-menu " aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="tbLibrosDisponibles.php">Tabla De Libros Disponibles</a></li>
-                        <li><a class="dropdown-item" href="tbLibrosPrestados.php">Tabla De Libros Prestados</a></li>
-                        <li><a class="dropdown-item" href="tbEquiposDisponibles.php">Tabla De equipos Disponibles</a></li>
-                        <li><a class="dropdown-item" href="tbEquiposPrestados.php">Tabla De Equipos Prestados</a></li>
-                        <li><a class="dropdown-item" href="tbUsuarios.php">Tabla De Usuarios</a></li>
+                   
 
-                      </ul>
-                    </li>
                     <li class="nav-item dropdown" >
                       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Buscar
                       </a>
+
                       <ul class="dropdown-menu " aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="buscarLibroGenero.php">Buscar Libro por Genero</a></li>
-                        <li><a class="dropdown-item" href="buscarLibroAutor.php ">Buscar Libro por Autor</a></li>
+                        <li><a class="dropdown-item" href="buscarUsuario.php">Buscar Usuario</a></li>
+                        <li><a class="dropdown-item" href="buscarConvocatoria.php ">Buscar Convocatorias</a></li>
+                        <li><a class="dropdown-item" href="buscarOfertasLab.php ">Buscar Ofertas Laborales</a></li>                        
                       </ul>
-                    </li>
-                      
-                    
-                    </li>
-                    
-                  </ul>
-                  
-                  <button class="btn btn-outline-success" type="submit"  name="fbtn" value="cerrar">Cerrar Seccion</button>
-               
+                    </li>                     
+                                   
+                  <button class="btn btn-outline-success" type="submit"  name="fbtn" value="cerrar">Cerrar Seccion</button>               
 
                 </div>
               </div>
@@ -87,8 +76,8 @@
           <div id="fondo"><img id="im" src="../imagenes/logosistema.png">
           </div>
         </form>
-
 </div>
+
   </nav>
   <script src="../boodstrap/bootstrap.bundle.js"></script>  
 </body>
